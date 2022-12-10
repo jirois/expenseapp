9938213e631a88b9f8ac8ec7900007c2dc447b09
Omas Ajiri <omasajiri@gmail.com>
2022-12-10 21:33:29 +0100

    fix:added listview

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -8,46 +8,47 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-        children: transactions.map((tx) {
-      return Card(
-        child: Row(
-          children: [
-            Container(
-              margin: EdgeInsets.symmetric(
-                horizontal: 15,
-                vertical: 10,
-              ),
-              decoration: BoxDecoration(
-                  border: Border.all(
-                color: Colors.orange,
-                width: 2,
-              )),
-              padding: EdgeInsets.all(10),
-              child: Text(
-                tx.amount.toString(),
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 14,
-                    color: Colors.orange),
-              ),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  tx.title,
-                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+    return ListView(
+      children: transactions.map((tx) {
+        return Card(
+          child: Row(
+            children: [
+              Container(
+                margin: EdgeInsets.symmetric(
+                  horizontal: 15,
+                  vertical: 10,
                 ),
-                Text(
-                  tx.date.toString(),
-                  style: TextStyle(color: Colors.grey),
-                )
-              ],
-            )
-          ],
-        ),
-      );
-    }).toList());
+                decoration: BoxDecoration(
+                    border: Border.all(
+                  color: Colors.orange,
+                  width: 2,
+                )),
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  tx.amount.toString(),
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 14,
+                      color: Colors.orange),
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    tx.title,
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    tx.date.toString(),
+                    style: TextStyle(color: Colors.grey),
+                  )
+                ],
+              )
+            ],
+          ),
+        );
+      }).toList(),
+    );
   }
 }
